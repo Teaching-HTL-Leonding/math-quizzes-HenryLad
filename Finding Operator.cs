cecd5793cf30a95eed0d28397fbad5cd497c8669
Henry Lad <113623013+HenryLad@users.noreply.github.com>
2022-12-01 11:26:32 +0100

    Add files via upload

---
+++ Finding Operator.cs
@@ -0,0 +1,34 @@
+﻿
+
+System.Console.WriteLine("Enter your first number");
+double firstnumber = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Enter your second number");
+double secondnumber = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Enter you result");
+double result = double.Parse(Console.ReadLine()!);
+
+if(firstnumber + secondnumber == result){
+    System.Console.WriteLine("You used the [+] operator");
+}
+if(firstnumber - secondnumber == result){
+    System.Console.WriteLine("You used the [-] operator");
+}
+if(secondnumber - firstnumber == result){
+    System.Console.WriteLine("You used the [-] operator");
+}
+if(secondnumber * firstnumber == result){
+    System.Console.WriteLine("You used an [*] operator");
+}
+if(firstnumber / secondnumber == result){
+    System.Console.WriteLine("You used the [/] operator");
+}
+if(secondnumber / firstnumber == result){
+    System.Console.WriteLine("You used the [/] operator");
+}
+if(firstnumber % secondnumber == result){
+    System.Console.WriteLine("You used an [%] operator");
+}
+if(secondnumber % firstnumber == result){
+    System.Console.WriteLine("You used an [%] operator");
+}
+
